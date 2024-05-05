7914d9e169fca010029e604067e546f87a449d04
hennano <scpscti@gmail.com>
2024-05-05 13:26:26 +0900

    異常系テストの追加

--- src/test/kotlin/net/hennabatch/dojinapi/controller/AuthorControllerTest.kt
+++ src/test/kotlin/net/hennabatch/dojinapi/controller/AuthorControllerTest.kt
@@ -82,5 +82,139 @@ class AuthorControllerTest: FunSpec({
             }
             confirmVerified(authorResponseMock)
         }
+
+        test("正常系_データなし"){
+            //準備
+            val authors = listOf<Author>()
+            val authorControllerLogicMock = mockk<AuthorControllerLogic>{
+                coEvery { fetchAllAuthors()} returns authors
+            }
+            val res = ResponseEntity(
+                statusCode = HttpStatusCode.OK,
+                responseBody = ResponseBody.JsonBody(JsonObject(mapOf()))
+            )
+            val authorResponseMock = mockk<AuthorResponse>{
+                every { makeAuthorListFetched(any()) } returns res
+
+            }
+            //モジュールの差し替え
+            mockkObject(objects = arrayOf(Module), recordPrivateCalls = true)
+            every {Module.koinModules()} returns module {
+                single<AuthorControllerLogic>{authorControllerLogicMock}
+                single<AuthorResponse>{authorResponseMock}
+                singleOf(::CommonResponse)
+            }
+
+            //実行(起動)
+            testApplication {
+                environment {
+                    config = ApplicationConfig("application_local.yaml")
+                }
+                // 実行(リクエスト)
+                client.get("/author"){
+                }.apply {
+                    //検証(リクエスト)
+                    logger.info(bodyAsText())
+                    status shouldBeEqual HttpStatusCode.OK
+                    bodyAsText() shouldBeEqual "{}"
+                }
+            }
+            //検証
+            coVerify(exactly = 1) {
+                authorControllerLogicMock.fetchAllAuthors()
+            }
+            confirmVerified(authorControllerLogicMock)
+            verify(exactly = 1) {
+                authorResponseMock.makeAuthorListFetched(any())
+            }
+            confirmVerified(authorResponseMock)
+        }
+
+        test("異常系_authorControllerLogicでエラー"){
+            //準備
+            val authors = listOf<Author>()
+            val authorControllerLogicMock = mockk<AuthorControllerLogic>{
+                coEvery { fetchAllAuthors()} throws Exception()
+            }
+            val authorResponseMock = mockk<AuthorResponse>{}
+            //モジュールの差し替え
+            mockkObject(objects = arrayOf(Module), recordPrivateCalls = true)
+            every {Module.koinModules()} returns module {
+                single<AuthorControllerLogic>{authorControllerLogicMock}
+                single<AuthorResponse>{authorResponseMock}
+                singleOf(::CommonResponse)
+            }
+
+            //実行(起動)
+            testApplication {
+                environment {
+                    config = ApplicationConfig("application_local.yaml")
+                }
+                // 実行(リクエスト)
+                client.get("/author"){
+                }.apply {
+                    //検証(リクエスト)
+                    logger.info(bodyAsText())
+                    status shouldBeEqual HttpStatusCode.InternalServerError
+                    bodyAsText() shouldBeEqual "{\"error\":\"ServerError\"}"
+                }
+            }
+            //検証
+            coVerify(exactly = 1) {
+                authorControllerLogicMock.fetchAllAuthors()
+            }
+            confirmVerified(authorControllerLogicMock)
+            verify(exactly = 0) {
+                authorResponseMock.makeAuthorListFetched(any())
+            }
+            confirmVerified(authorResponseMock)
+        }
+
+        test("AuthorResponseでエラー"){
+            //準備
+            val authors = listOf(
+                Author(1, "test1", null, listOf(), null, null),
+                Author(2, "test2", null, listOf(), null, null),
+                Author(3, "test3", null, listOf(), null, null)
+            )
+            val authorControllerLogicMock = mockk<AuthorControllerLogic>{
+                coEvery { fetchAllAuthors()} returns authors
+            }
+            val authorResponseMock = mockk<AuthorResponse>{
+                every { makeAuthorListFetched(any()) } throws Exception()
+
+            }
+            //モジュールの差し替え
+            mockkObject(objects = arrayOf(Module), recordPrivateCalls = true)
+            every {Module.koinModules()} returns module {
+                single<AuthorControllerLogic>{authorControllerLogicMock}
+                single<AuthorResponse>{authorResponseMock}
+                singleOf(::CommonResponse)
+            }
+
+            //実行(起動)
+            testApplication {
+                environment {
+                    config = ApplicationConfig("application_local.yaml")
+                }
+                // 実行(リクエスト)
+                client.get("/author"){
+                }.apply {
+                    //検証(リクエスト)
+                    logger.info(bodyAsText())
+                    status shouldBeEqual HttpStatusCode.InternalServerError
+                    bodyAsText() shouldBeEqual "{\"error\":\"ServerError\"}"
+                }
+            }
+            //検証
+            coVerify(exactly = 1) {
+                authorControllerLogicMock.fetchAllAuthors()
+            }
+            confirmVerified(authorControllerLogicMock)
+            verify(exactly = 1) {
+                authorResponseMock.makeAuthorListFetched(any())
+            }
+            confirmVerified(authorResponseMock)
+        }
     }
 })
